4f8d2260d5d198012be0d3bbb634f043226a77df
Xavier Ortiz <cuchumino@gmail.com>
2016-12-13 14:31:23 -0500

    set TTL to 1

--- first-openmix-app.js
+++ first-openmix-app.js
@@ -10,24 +10,22 @@ var round = 0;
 
 function onRequest(request, response) {
     // cycles through all servers I want to cycle through.
-    if (round == 0){
+    if (round === 0){
         response.respond('akamai_object_delivery', '4.4.4.4');       
     }
     
-    else if (round == 1) {
+    else if (round === 1) {
         response.respond('aws_ec2_us_east_va', '8.8.8.8'); 
     }
     
-    else if (round == 2) {
+    else if (round === 2) {
         response.respond('aws_ec2_us_east_va', '8.8.4.4'); 
     }
     
     else {
         
     }
-    
-    response.setTTL(20);
+    response.setTTL(1);
     round = (round + 1) % 3;
-    
 }
 
