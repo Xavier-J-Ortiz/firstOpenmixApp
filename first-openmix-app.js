1f0a8c2f5fbabc8fe4f2394eaf80e31b256b4ba9
Xavier Ortiz <cuchumino@gmail.com>
2016-12-13 14:07:28 -0500

    first app, trying to see if round robin will work on server. Might not. Will add comments in next commit.

--- first-openmix-app.js
+++ first-openmix-app.js
@@ -1,6 +1,32 @@
 function init(config) {
-    // foo, bar and baz are provider aliases
+    
     config.requireProvider('akamai_object_delivery');
     config.requireProvider('aws_ec2_us_east_va');
     config.requireProvider('cloudflare_cdn');
 }
+
+var round = 0;
+
+function onRequest(request, response) {
+    
+    if (round == 0){
+        response.respond('akamai_object_delivery', '4.4.4.4');       
+    }
+    
+    else if (round == 1) {
+        response.respond('aws_ec2_us_east_va', '8.8.8.8'); 
+    }
+    
+    else if (round == 2) {
+        response.respond('aws_ec2_us_east_va', '8.8.4.4'); 
+    }
+    
+    else {
+        
+    }
+    
+    response.setTTL(20);
+    round = (round + 1) % 3;
+    
+}
+
