894cc1df8ca82b0f44cee5c81803f62be8ab37dc
Xavier Ortiz <cuchumino@gmail.com>
2016-12-28 12:34:52 -0500

    Used JSLint to correct some issues. added unit_test.html but doesn't do anything just yet and is useless for the time being.

--- first-openmix-app.js
+++ first-openmix-app.js
@@ -1,6 +1,7 @@
 //Xavier's first app
 function init(config) {
     // useless for the time being
+    'use strict';
     config.requireProvider('akamai_object_delivery');
     config.requireProvider('aws_ec2_us_east_va');
     config.requireProvider('cloudflare_cdn');
@@ -10,28 +11,25 @@ var round = 0;
 
 function onRequest(request, response) {
     // cycles through all servers I want to cycle through.
-    if (round === 0){
+    'use strict';
+    if (round === 0) {
         // response.respond('akamai_object_delivery', '4.4.4.4');
         response.addARecord('1.1.1.1');
         response.addARecord('2.2.2.2');
         response.addARecord('3.3.3.3');
         response.addARecord('4.4.4.4');
         response.setTTL(20);
-    }
-    
-    else if (round === 1) {
+    } else if (round === 1) {
         // why is specifying the provider alias important?
         response.respond('aws_ec2_us_east_va', '8.8.8.8');
         response.setTTL(30);
-    }
-    
-    else if (round === 2) {
+    } else if (round === 2) {
         //response.respond('akamai_object_delivery', '8.8.4.4');
         //response.setTTL(20);
 
         response.setProvider('cloudflare_cdn');
-        response.addCName('bar.foo.com')
-        response.setTTL(25)
+        response.addCName('bar.foo.com');
+        response.setTTL(25);
     }
     
     round = (round + 1) % 3;
